48a8b459d538550721eb4b424b254dc930e774de
TobySmith2019 <51064548+TobySmith2019@users.noreply.github.com>
2021-06-28 11:57:28 +1200

    Add files via upload
    completed first level map and started creating external js file for player

---
+++ L3-Programming/player.js
@@ -0,0 +1,22 @@
+class Player {
+    constructor(x, y, w, h, c, xSpeed, ySpeed) {
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+      this.c = c;
+      this.xSpeed = xSpeed;
+      this.ySpeed = ySpeed;
+    }
+  
+    drawRect() {
+      canvasContext.fillStyle = this.c;
+      canvasContext.fillRect(this.x, this.y, this.w, this.h);
+    }
+
+    move() {
+        if (dKeyPressed) {
+            var playerLocation = arrayMap[playerLocation];
+        }
+    }
+}
